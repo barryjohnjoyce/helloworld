94a2caef2817091cdaf3f7b6250226670fe393a3
Barry Joyce <barryjohnjoyce@gmail.com>
2016-10-05 16:32:51 +0100

    third commit

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,7 @@
-name=raw_input("hi what is your name")
-print "hello, %s" % name
+name = raw_input("hi, what is your name?")
+age = raw_input("and what is your age?")
+
+print "Hello, %s!" % name #in this context modulus means "take this string and put it into another string"
+
+print "Hello, {0}! so, you are {1} years old!".format(name,age)
 
